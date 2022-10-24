d29779ffc6c1ee975883fd7d5207425d9b019946
DrasticCoder <you@example.com>
2022-10-24 22:26:02 +0530

    Host db on atlas

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const validUrl = require('valid-url');
 const app = express();
 app.use(express.json())
 
+const URL = require('./Model/Url');
+
+// Connecting to DB
+mongoose.connect('mongodb+srv://test:test@drasticcoder.sk0i8sj.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>console.log('Connected to DB'))
+
 const port = 3000;
 app.listen(port,()=>{
     console.log(`Server is up and running on port : ${port}`);

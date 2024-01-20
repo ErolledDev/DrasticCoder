3f259a8c63823ca63768e25828ea80bac056a647
Deep Bansode <drasticoder@gmail.com>
2024-01-21 01:07:28 +0530

    Refactor: server.js & render using ejs

--- server.js
+++ server.js
@@ -2,62 +2,102 @@ const express = require('express');
 const mongoose = require('mongoose');
 const shortId = require('shortid');
 const validUrl = require('valid-url');
+const path = require('path');
+const ejs = require('ejs');
+
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production') dotenv.config({ path: './config.env' });
 
 const app = express();
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 const URL = require('./Model/Url');
 
 // Connecting to DB
-mongoose.connect('mongodb+srv://test:test@drasticcoder.sk0i8sj.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to DB'))
 
 // Routes
-app.post('/', (req, res) => {
-    const link = req.body.url;
-    const ID = shortId.generate()
-
-    const SHORT_ID = new URL({
-        longUrl: link,
-        shortUrl: `https://lits.rocks/${ID}`,
-        shortIdentity: ID,
-        date: Date.now()
-    })
-    SHORT_ID.save((error, results) => {
-
-        if (results) {
-            if (validUrl.isUri(link)) {
-                res.status(200).json(SHORT_ID)
-            } else {
-                res.status(401).json('Please check the URL')
-            }
-        }
-        if (error) {
-            res.status(500).json('something went wrong...');
-            }
-    })
-
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+app.get("/add",(req,res)=>{
+    res.render("add",{message:""});
 })
 
-app.get('/', (req, res) => {
-    try {
-        URL.find({}, (error, urls) => {
-            if (!error) {
-                res.status(200).json(urls)
-            } else {
-                console.log(error)
+app.get('/add-link', async(req, res) => {
+    let {url,identifier,password } = req.query;
+
+    let message="123";
+
+    if (!url) {
+        message = "Please enter a valid URL"
+    }
+
+    // if (validUrl.isUri(url)) {
+    //    return res.status(401).json('Please check the URL')
+    // }
+    
+    if(identifier){
+        URL.findOne({ shortIdentity: identifier }, (error, result) => {
+            if (result) {
+                message = "short link already exists"
+                // return res.status(401).json('short link already exists')
             }
         })
-    } catch (err) {
-        console.log(err);
+    } else {
+        const ID = shortId.generate()
+        identifier = ID
     }
+
+    const _url = {
+        longUrl: url,
+        shortUrl: `${process.env.BACKEND_URL}/${identifier}`,
+        shortIdentity: identifier,
+    }
+
+    if(password){
+        _url.password = password
+    }
+
+    const result = await URL.create(_url);
+    
+    if(result){
+        message = "link generated successfully"
+        //    return res.status(200).json({message:"link generated successfully",result})
+    } else{
+        message = "something went wrong..."
+        // return  res.status(500).json('something went wrong...')
+    }
+    
+    return res.render("add",{message})
 })
+
+// TODO: edit route
+app.post('/edit/:id', async(req, res) => {
+        const id = req.params.id;
+        const pass = req.query.password;
+    
+        const foundURL = await URL.findOne({ shortIdentity: id });
+    
+        if (!foundURL) {
+            return res.status(401).json('Please enter a valid URL')
+        }
+       if(foundURL.password == pass){
+            return res.render("edit")
+       } else {
+            return res.status(401).json('Please enter a valid password')
+       }
+    })
 
 app.get('/:id', async (req, res) => {
     try {
         const foundURL = await URL.findOne({ shortIdentity: req.params.id });
         if (!foundURL) {
-            res.status(404).json('URL not found...!');
+            res.redirect("/");
         } else {
             res.redirect(foundURL.longUrl)
         }
@@ -67,6 +107,7 @@ app.get('/:id', async (req, res) => {
 })
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Server is up and running on port : ${port}`);
 })
